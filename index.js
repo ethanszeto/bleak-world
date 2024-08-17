02fcec999f8aa520e8b2f0c257aa2eb674e6111e
Ethan Szeto <ethan.szeto@gmail.com>
2024-08-17 17:38:19 -0400

    removed old perlin generator iterations

--- index.js
+++ index.js
@@ -7,23 +7,24 @@ const draw = () => {
 
   const ctx = canvas.getContext("2d");
 
-  //   for (let x = 0; x < canvas.width; x += 30) {
-  //     new Box(x, 0, 1, canvas.height, rgba(255, 255, 255, 1)).draw(ctx);
-  //   }
+  for (let x = 0; x < canvas.width; x += 30) {
+    new Box(x, 0, 1, canvas.height, rgba(255, 255, 255, 1)).draw(ctx);
+  }
 
-  //   for (let y = 0; y < canvas.height; y += 30) {
-  //     new Box(0, y, canvas.width, 1, rgba(255, 255, 255, 1)).draw(ctx);
-  //   }
+  for (let y = 0; y < canvas.height; y += 30) {
+    new Box(0, y, canvas.width, 1, rgba(255, 255, 255, 1)).draw(ctx);
+  }
   const perlin = new PerlinNoiseGenerator();
-  perlin.setSeed(12835618);
-  perlin.setScale(0.001, 0.005, 0.005);
+  perlin.setSeed(111634516782378);
+  //   perlin.setSeed(parseInt(Math.random() * 1000000));
+  perlin.setScale(0.001, 0.003, 0.005);
 
   const discrete = true;
 
-  for (var y = 0; y < canvas.height; y += 5) {
-    for (var x = 0; x < canvas.width; x += 5) {
+  for (var y = 0; y < canvas.height; y += 4) {
+    for (var x = 0; x < canvas.width; x += 4) {
       if (discrete) {
-        var v = perlin.getIntensity(x + 0.5, y + 0.5) < 0.5 ? 0 : 1;
+        var v = perlin.getIntensity(x, y) < 0.5 ? 0 : 1;
         console.log(x, y, v);
         new Box(x, y, 10, 10, rgba(255, 255, 255, v)).draw(ctx);
       } else {
@@ -33,29 +34,6 @@ const draw = () => {
       }
     }
   }
-
-  //   for (var y = 0; y < canvas.height; y += 30) {
-  //     for (var x = 0; x < canvas.width; x += 30) {
-  //       var v = parseInt(Math.random() * 255);
-  //       new Box(Math.floor(x), Math.floor(y), 30, 30, rgba(v, v, v, 1)).draw(ctx);
-  //     }
-  //   }
-
-  //   const circle1 = new Circle(500, 500, 100, rgba(255, 0, 0, 1));
-  //   const circle2 = new Circle(300, 300, 75, rgba(0, 255, 1, 1));
-
-  //   console.log(circle1.collidesWith(circle2));
-
-  //   const box1 = new Box(100, 100, 60, 60, "green");
-  //   const box2 = new Box(200, 200, 80, 80, "yellow");
-
-  //   console.log(box2.collidesWith(circle2));
-
-  //   circle1.draw(ctx);
-  //   circle2.draw(ctx);
-
-  //   box1.draw(ctx);
-  //   box2.draw(ctx);
 };
 
 window.addEventListener("load", draw);
--- src/fns/perlin.js
+++ src/fns/perlin.js
@@ -1,11 +1,11 @@
 class PerlinNoiseGenerator {
   constructor() {
-    this.permutation = [];
+    this.permutation = Array.from({ length: 256 }, (_, i) => i);
     this.perm = [];
     this.seed = 0;
-    this.baseScale = 1; // Default base scale
-    this.variationScale = 0.1; // Scale of the variation noise
-    this.variationIntensity = 0; // Intensity of the variation
+    this.baseScale = 1;
+    this.variationScale = 0.1;
+    this.variationIntensity = 0;
     this.setSeed(Math.random());
   }
 
@@ -52,7 +52,6 @@ class PerlinNoiseGenerator {
   noise(x, y) {
     const X = Math.floor(x) & 255;
     const Y = Math.floor(y) & 255;
-
     x -= Math.floor(x);
     y -= Math.floor(y);
 
@@ -76,215 +75,12 @@ class PerlinNoiseGenerator {
   }
 
   getIntensity(x, y) {
-    // Generate variation noise to modulate the scale
     const variationNoise = this.noise(x * this.variationScale, y * this.variationScale);
     const effectiveScale = this.baseScale + this.variationIntensity * variationNoise;
 
-    // Scale the coordinates before generating noise
     x *= effectiveScale;
     y *= effectiveScale;
 
-    // Noise value is typically in the range [-1, 1], normalize to [0, 1]
     return (this.noise(x, y) + 1) / 2;
   }
 }
-
-// class PerlinNoiseGenerator {
-//   constructor() {
-//     this.permutation = [];
-//     this.perm = [];
-//     this.seed = 0;
-//     this.scale = 1; // Default scale
-//     this.setSeed(Math.random());
-//   }
-
-//   setSeed(seed) {
-//     this.seed = seed;
-//     this.permutation = [];
-
-//     // Create a permutation table
-//     for (let i = 0; i < 256; i++) {
-//       this.permutation[i] = i;
-//     }
-
-//     // Shuffle the permutation table based on the seed
-//     for (let i = 255; i > 0; i--) {
-//       const rand = Math.floor((this.seed * 1000) % (i + 1));
-//       [this.permutation[i], this.permutation[rand]] = [this.permutation[rand], this.permutation[i]];
-//     }
-
-//     // Double the permutation table to avoid overflow
-//     this.perm = this.permutation.concat(this.permutation);
-//   }
-
-//   setScale(scale) {
-//     this.scale = scale;
-//   }
-
-//   fade(t) {
-//     return t * t * t * (t * (t * 6 - 15) + 10);
-//   }
-
-//   lerp(t, a, b) {
-//     return a + t * (b - a);
-//   }
-
-//   grad(hash, x, y) {
-//     const h = hash & 3;
-//     const u = h < 2 ? x : y;
-//     const v = h < 2 ? y : x;
-//     return (h & 1 ? -u : u) + (h & 2 ? -v : v);
-//   }
-
-//   noise(x, y) {
-//     const X = Math.floor(x) & 255;
-//     const Y = Math.floor(y) & 255;
-
-//     x -= Math.floor(x);
-//     y -= Math.floor(y);
-
-//     const u = this.fade(x);
-//     const v = this.fade(y);
-
-//     const aa = this.perm[X] + Y;
-//     const ab = this.perm[X] + Y + 1;
-//     const ba = this.perm[X + 1] + Y;
-//     const bb = this.perm[X + 1] + Y + 1;
-
-//     const gradAA = this.grad(this.perm[aa], x, y);
-//     const gradBA = this.grad(this.perm[ba], x - 1, y);
-//     const gradAB = this.grad(this.perm[ab], x, y - 1);
-//     const gradBB = this.grad(this.perm[bb], x - 1, y - 1);
-
-//     const lerpX1 = this.lerp(u, gradAA, gradBA);
-//     const lerpX2 = this.lerp(u, gradAB, gradBB);
-
-//     return this.lerp(v, lerpX1, lerpX2);
-//   }
-
-//   getIntensity(x, y) {
-//     // Scale the coordinates before generating noise
-//     x *= this.scale;
-//     y *= this.scale;
-
-//     // Noise value is typically in the range [-1, 1], normalize to [0, 1]
-//     return (this.noise(x, y) + 1) / 2;
-//   }
-// }
-
-// class PerlinNoiseGenerator {
-//   constructor() {
-//     this.permutation = [];
-//     this.perm = [];
-//     this.seed = 0;
-//     this.setSeed(Math.random());
-//   }
-
-//   setSeed(seed) {
-//     this.seed = seed;
-//     this.permutation = [];
-
-//     // Create a permutation table
-//     for (let i = 0; i < 256; i++) {
-//       this.permutation[i] = i;
-//     }
-
-//     // Shuffle the permutation table based on the seed
-//     for (let i = 255; i > 0; i--) {
-//       const rand = Math.floor((this.seed * 1000) % (i + 1));
-//       [this.permutation[i], this.permutation[rand]] = [this.permutation[rand], this.permutation[i]];
-//     }
-
-//     // Double the permutation table to avoid overflow
-//     this.perm = this.permutation.concat(this.permutation);
-//   }
-
-//   fade(t) {
-//     return t * t * t * (t * (t * 6 - 15) + 10);
-//   }
-
-//   lerp(t, a, b) {
-//     return a + t * (b - a);
-//   }
-
-//   grad(hash, x, y) {
-//     const h = hash & 3;
-//     const u = h < 2 ? x : y;
-//     const v = h < 2 ? y : x;
-//     return (h & 1 ? -u : u) + (h & 2 ? -v : v);
-//   }
-
-//   noise(x, y) {
-//     const X = Math.floor(x) & 255;
-//     const Y = Math.floor(y) & 255;
-
-//     x -= Math.floor(x);
-//     y -= Math.floor(y);
-
-//     const u = this.fade(x);
-//     const v = this.fade(y);
-
-//     const aa = this.perm[X] + Y;
-//     const ab = this.perm[X] + Y + 1;
-//     const ba = this.perm[X + 1] + Y;
-//     const bb = this.perm[X + 1] + Y + 1;
-
-//     const gradAA = this.grad(this.perm[aa], x, y);
-//     const gradBA = this.grad(this.perm[ba], x - 1, y);
-//     const gradAB = this.grad(this.perm[ab], x, y - 1);
-//     const gradBB = this.grad(this.perm[bb], x - 1, y - 1);
-
-//     const lerpX1 = this.lerp(u, gradAA, gradBA);
-//     const lerpX2 = this.lerp(u, gradAB, gradBB);
-
-//     return this.lerp(v, lerpX1, lerpX2);
-//   }
-
-//   getIntensity(x, y) {
-//     // Noise value is typically in the range [-1, 1], normalize to [0, 1]
-//     return (this.noise(x, y) + 1) / 2;
-//   }
-// }
-
-// const perlin = {
-//   rand_vect: function () {
-//     let theta = Math.random() * 2 * Math.PI;
-//     return { x: Math.cos(theta), y: Math.sin(theta) };
-//   },
-//   dot_prod_grid: function (x, y, vx, vy) {
-//     let g_vect;
-//     let d_vect = { x: x - vx, y: y - vy };
-//     if (this.gradients[[vx, vy]]) {
-//       g_vect = this.gradients[[vx, vy]];
-//     } else {
-//       g_vect = this.rand_vect();
-//       this.gradients[[vx, vy]] = g_vect;
-//     }
-//     return d_vect.x * g_vect.x + d_vect.y * g_vect.y;
-//   },
-//   smootherstep: function (x) {
-//     return 6 * x ** 5 - 15 * x ** 4 + 10 * x ** 3;
-//   },
-//   interp: function (x, a, b) {
-//     return a + this.smootherstep(x) * (b - a);
-//   },
-//   seed: function () {
-//     this.gradients = {};
-//     this.memory = {};
-//   },
-//   get: function (x, y) {
-//     if (this.memory.hasOwnProperty([x, y])) return this.memory[[x, y]];
-//     let xf = Math.floor(x);
-//     let yf = Math.floor(y);
-//     //interpolate
-//     let tl = this.dot_prod_grid(x, y, xf, yf);
-//     let tr = this.dot_prod_grid(x, y, xf + 1, yf);
-//     let bl = this.dot_prod_grid(x, y, xf, yf + 1);
-//     let br = this.dot_prod_grid(x, y, xf + 1, yf + 1);
-//     let xt = this.interp(x - xf, tl, tr);
-//     let xb = this.interp(x - xf, bl, br);
-//     let v = this.interp(y - yf, xt, xb);
-//     this.memory[[x, y]] = v;
-//     return v;
-//   },
-// };
